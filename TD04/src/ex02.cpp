dea50cf6f643b2002858f10751d7f9525aad3898
LisaMrc <lisa.muracciole77@gmail.com>
2024-04-04 23:08:21 +0200

    🪚 TD04 ex02

--- TD04/src/ex02.cpp
+++ TD04/src/ex02.cpp
@@ -15,28 +15,41 @@ int nbr_letters(std::string const& sentence)
 
 std::vector<std::string> split_string(std::string const& str)
 {
-    std::vector<std::string>splitted_string{};
-    int idx{};
+    std::vector<std::string> output{};
+    int i{};
+    auto idx_begin{str.begin()};
+    auto idx_end{str.begin()};
 
-    for (size_t i = 0; i < str.size(); i++)
+    while (i != str.size())
     {
-        auto Jean = find_if(str.begin() + idx, str.end(), is_space);
-        int nbr_letters = std::distance(idx, Jean);
-        std::string tmp_str{str[idx], str[nbr_letters]};
-        splitted_string.push_back(tmp_str);
+        auto idx_end = find_if(idx_begin, str.end(), is_space);
+        int nbr_letters = std::distance(idx_begin, idx_end);
+        std::string tmp_str{str[idx_begin], str[nbr_letters]};
+        // TODO : corriger le bug ici
+        output.push_back(tmp_str);
         tmp_str.clear();
-
-        idx += Jean;
+        idx_end += nbr_letters;
+        i+=nbr_letters+1;
     }
-    
-    // TODO : idx et Jean à renommer en idx_begin et idx_end pour y voir + clair
 
-    return splitted_string;
+    return output;
 }
 
 int main()
 {
-    std::cout << nbr_letters("hello world") << std::endl;
     split_string("hello world");
     return 0;
 }
+
+// PROTOCOL
+// init une sortie
+//  on a un indice end et begin, init tous les 2
+
+// tant que i != str.size()
+//  auto idx_end = find_if(indice_begin, str.end, xxxx)
+//  nbr_letters == distance(indice_begin, idx_end, xxxx)
+//  creer une string temporaire tmp_str qui contient les lettres de indice_begin à indice end (donc de indice_begin pendant nbr_letters de lettres)
+// ranger la str tmp dans le tableau output
+// clear la string temporaire
+//  idx_begin += nbr_letters
+// i+= nbr_letters+1
