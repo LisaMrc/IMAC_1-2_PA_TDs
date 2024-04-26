2a767c9d7908c975c56f675dda27bfae6828e9f8
Lisa MURACCIOLE <lisa.muracciole77@gmail.com>
2024-04-26 08:56:42 +0200

    🪵TD05 ex03

--- TD05/src/ex03.cpp
+++ TD05/src/ex03.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 enum class CardKind
 {
@@ -56,7 +57,6 @@ struct Card
         default:
             break;
         }
-
         return cardHash;
     }
 };
@@ -89,4 +89,46 @@ std::vector<Card> get_cards(size_t const size)
         cards.push_back({static_cast<CardKind>(rand() % 4), static_cast<CardValue>(rand() % 13)});
     }
     return cards;
+}
+
+std::string card_name(Card const& card)
+{
+    std::string name {};
+    unsigned int card_value {(static_cast<unsigned int>(card.value)+2) % 14};
+
+    if (card_value < 10) {
+        name += '0' + card_value;
+    }else if (card_value == 10) {
+        name += "10";
+    }else if (card_value == 11) {
+        name += 'V';
+    }else if (card_value == 12) {
+        name += 'Q';
+    }else if (card_value == 13) {
+        name += 'K';
+    }
+
+    name += " of ";
+
+    if (card.kind == CardKind::Heart) {
+        name += "Heart";
+    }else if (card.kind == CardKind::Diamond) {
+        name += "Diamond";
+    }else if (card.kind == CardKind::Club) {
+        name += "Club";
+    }else if (card.kind == CardKind::Spade) {
+        name += "Spade";
+    }
+
+    return name;
+}
+
+int main()
+{
+    std::vector<Card> deck = get_cards(100);
+
+    for (Card card : deck)
+    {
+        std::cout << card_name(card) << std::endl;
+    }
 }
