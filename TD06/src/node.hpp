569937d04d9349a98beb98a39eb903182fe7c1f1
Lisa MURACCIOLE <lisa.muracciole77@gmail.com>
2024-05-03 10:35:59 +0200

    🪚 TD06 ex01

--- TD06/src/node.hpp
+++ TD06/src/node.hpp
@@ -74,25 +74,75 @@ struct Node
     void Node::display_infixe() const 
     {
         if(left) left->display_infixe();
-        if(right) right->display_infixe();
-
         std::cout << this->value << std::endl;
+        if(right) right->display_infixe();
     }
 
-    std::vector<Node const *> Node::prefixe()  const 
+    std::vector<Node const *> Node::prefixe() const 
     {
-        std::vector<Node const*> node_list{};
-        node_list.push_back(this);
+        std::vector<Node const*> nodes{};
+        nodes.push_back(this);
 
-       if (!is_leaf())
+        if(left)
         {
-            if(left) left->prefixe();
-            if(right) right->prefixe();
+            auto left_nodes {left->prefixe()};
+            nodes.insert(nodes.end(), left_nodes.begin(), left_nodes.end());
         }
 
-        return node_list;
+        if(right)
+        {
+            auto right_nodes {right->prefixe()};
+            nodes.insert(nodes.end(), right_nodes.begin(), right_nodes.end());
+        }
+
+        return nodes;
     }
 };
+
+Node*& most_left(Node*& node)
+{
+    if (node->left->left != nullptr)
+    {
+        most_left(node->left);
+    }
+    else
+    {
+        return node->left->left;
+    }
+
+    return node;
+}
+
+bool remove(Node*& node, int value)
+{
+    if (value == node->value && node->is_leaf())
+    {
+        // On supprime le nœud courant
+        delete node;
+        // Comme on a une référence sur le pointeur du nœud courant, on le met à jour avec nullptr
+        // Ainsi le parent du nœud courant aura un pointeur vers nullptr
+        node = nullptr;
+        // on retourne true car la suppression a été effectuée
+        return true;
+    }
+    else if (value == node->value && (node->right == nullptr && node->left != nullptr))
+    {
+        auto tmp_L_Value = node->left->value;
+        node->left->delete_childs();
+        node->value = tmp_L_Value;
+    }
+    else if (value == node->value && ((node->right != nullptr && node->right != nullptr)))
+    {
+        auto tmp_most_L_value = most_left(node->right)->value;
+        remove(most_left(node->right), tmp_most_L_value);
+        node->value = tmp_most_L_value;
+    }
+
+    /* 
+    TODO : écrire condition child right (OU)
+    TODO : inclure code précédent dans dichotomie où on cherche la valeur dans l'arbre
+    */ 
+}
 
 void pretty_print_left_right(Node const& node, std::string const& prefix, bool is_left);
 void pretty_print_left_right(Node const& node);
