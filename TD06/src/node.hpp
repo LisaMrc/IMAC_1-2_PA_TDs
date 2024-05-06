235a642fc0aca080eac846cabd153d47033ab7a6
Lisa MURACCIOLE <lisa.muracciole77@gmail.com>
2024-05-06 17:51:41 +0200

    🪵 TD06 ex02
    Link error but no red squiggles - Needs to be improved

--- TD06/src/node.hpp
+++ TD06/src/node.hpp
@@ -9,167 +9,21 @@ struct Node
     Node* left { nullptr };
     Node* right { nullptr };
 
-    bool is_leaf() const
-    {
-        if (this->left == nullptr && this->right == nullptr)
-            return true;
-        return false; 
-    }
+    bool is_leaf() const;
+    void insert(int value);
+    int Node::height() const;
 
-    void Node::insert(int value)
-    {
-        if (value < Node::value)
-        {
-            left = create_node(value);
-        }
-        else
-        {
-            right = create_node(value);
-        }
-    }
+    void delete_childs();
 
-    int Node::height() const
-    {
-        int left_height{0};
-        int right_height{0};
+    void display_infixe() const;
+    std::vector<Node const *> prefixe() const;
 
-        if (this->left != nullptr)
-        {
-            left_height = left->height();
-        }
-
-        if (this->right != nullptr)
-        {
-            right_height = right->height();
-        }
-
-        if (left_height >= right_height)
-        {
-            return left_height + 1;
-        }
-
-        else
-        {
-            return right_height + 1;
-        }
-    } 
-
-    void Node::delete_childs() 
-    {
-        if(left)
-        {
-            left->delete_childs();
-            delete this->left;
-            left = nullptr;
-        }
-
-        if(right)
-        {
-            right->delete_childs();
-            delete this->right;
-            right = nullptr;
-        }
-    }
-
-    void Node::display_infixe() const 
-    {
-        if(left) left->display_infixe();
-        std::cout << this->value << std::endl;
-        if(right) right->display_infixe();
-    }
-
-    std::vector<Node const *> Node::prefixe() const 
-    {
-        std::vector<Node const*> nodes{};
-        nodes.push_back(this);
-
-        if(left)
-        {
-            auto left_nodes {left->prefixe()};
-            nodes.insert(nodes.end(), left_nodes.begin(), left_nodes.end());
-        }
-
-        if(right)
-        {
-            auto right_nodes {right->prefixe()};
-            nodes.insert(nodes.end(), right_nodes.begin(), right_nodes.end());
-        }
-
-        return nodes;
-    }
+    void insert_from_root(int value);
 };
-
-Node*& most_left(Node*& node)
-{
-    if (node->left->left != nullptr)
-    {
-        most_left(node->left);
-    }
-    else
-    {
-        return node->left->left;
-    }
-
-    return node;
-}
-
-bool remove(Node*& node, int value)
-{
-    
-    if (value == node->value && node->is_leaf())
-    {
-        // On supprime le nœud courant
-        delete node;
-        // Comme on a une référence sur le pointeur du nœud courant, on le met à jour avec nullptr
-        // Ainsi le parent du nœud courant aura un pointeur vers nullptr
-        node = nullptr;
-        // on retourne true car la suppression a été effectuée
-        return true;
-    }
-    else if (value == node->value && (node->right == nullptr && node->left != nullptr))
-    {
-        auto tmp_L_Value = node->left->value;
-        node->left->delete_childs();
-        node->value = tmp_L_Value;
-    }
-    else if (value == node->value && ((node->right != nullptr && node->left == nullptr)))
-    {
-        auto tmp_R_Value = node->right->value;
-        node->left->delete_childs();
-        node->value = tmp_R_Value;
-    }
-    else if (value == node->value && (node->right != nullptr && node->left != nullptr))
-    {
-        auto tmp_most_L_value = most_left(node->right)->value;
-        remove(most_left(node->right), tmp_most_L_value);
-        node->value = tmp_most_L_value;
-    }
-    else if (value > node->value)
-    {
-        remove(node->right, value);
-    }
-    else if (value < node->value)
-    {
-        remove(node->left, value);
-    }
-    else
-    {
-        std::cout << "error" << std::endl;
-    }
-}
-
-void delete_tree(Node* node)
-{
-    /* first delete both subtrees */
-    delete_tree(node->left); 
-    delete_tree(node->right); 
-
-    /* then delete the node */
-    std::cout << "Deleting node : " << node->value << std::endl; 
-    delete node;
-}
 
 void pretty_print_left_right(Node const& node, std::string const& prefix, bool is_left);
 void pretty_print_left_right(Node const& node);
-
 Node* create_node(int value);
+Node*& most_left(Node*& node);
+bool remove(Node*& node, int value);
+void delete_tree(Node* node);
